79221d56ccd339baf17bdcc0bde149492864451b
Andrew Fleck <andrewfleck2@cmail.carleton.ca>
2013-10-01 17:44:05 -0400

    Finalizing some things, declaring global variable
    removed prompt from within game function

--- Serverpi.py
+++ Serverpi.py
@@ -4,7 +4,6 @@
 import socket
 import re
 
-userinput = ""
 pifaceround = 0
 gertpiround = 0
 pifacetotaltime = 0
@@ -13,11 +12,16 @@ gertpitotaltime = 0
 
 
 def parsestring(stringin):
+    global pifacetotaltime
+    global gertpitotaltime
+    global pifaceround
+    global gertpiround
+    
     r = re.match("^.*'(\w+)'(.*)$", stringin)          #parses incoming string from client pi
     if r:
         i,v = r.groups()
         if (i=="piface"):                              #determines which pi it came from
-            pifacetotaltime=pifacetotaltime+int(v)          #
+            pifacetotaltime=pifacetotaltime+int(v)          
             pifaceround=pifaceround+1
         elif (i=="gertpi"):
             gertpitotaltime=gertpitotaltime+int(v)
@@ -27,11 +31,11 @@ def parsestring(stringin):
 
 
 def instructions():
-    print "There are two raspberrypi's with peripheral boards attached. These function as controllers"
+    print "\n\nThere are two raspberrypi's with peripheral boards attached. These function as controllers"
     print "This serverpi will send commands to them telling them which LEDs to light"        
     print "It will count the time it takes to react to each LED by pressing the correct button on the controller pi's"
     print "The winner will be determined by the lower average reaction time over 10 rounds"
-    print "MOST IMPORTANTLY, HAVE FUN!"
+    print "MOST IMPORTANTLY, HAVE FUN!\n"
     
     
 ##
@@ -41,6 +45,11 @@ def instructions():
 # and keeps track of the total. When it receives ten results, it calculates and displays an average.
 ##
 def game():
+    global pifacetotaltime = 0
+    global gertpitotaltime = 0
+    global pifacerount = 0
+    global gertpiround = 0
+    
     # Setting up this pi as a server using the socket library
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -48,26 +57,23 @@ def game():
     s.listen(1)
     
     komm, addr = s.accept()            #The server waits for the client to connect                       
-    data = raw_input("Enter Command: ")       #Once the connection is made, the server can start the game
-    if data == "stop":                        #Or it can quit
-        pass
-    elif data == "start game":                #if the command is to start...
-        komm.send(data)                       #sends the start command to clients
-        while (True):
-            parsestring(komm.recv(1024))        #end_time is received from the client as a string type
-           # print "Waiting to receive response time..."
-            #print "received response in " + end_time + " milliseconds"               
-            #total_time = total_time + int(end_time)          #Sum the total time by changing the string end_time into an integer
-           # round += 1                                       #increment the round
-            if (pifaceround == 10 and gertpiround == 10):                     #when round reaches 10, 10 responses have been received
-                break                           #So we stop
-            komm, addr = s.accept()                          #Wait for the client to be ready to send another value
-        piface_avg_time = pifacetotaltime/10.0              #Calculate the average time in milliseconds
-        print "Piface average reaction time is: " + str(piface_avg_time) + " milliseconds"
-        gertpi_avg_time = gertpitotaltime/10.0              #Calculate the average time in milliseconds
-        print "Gertpi average reaction time is: " + str(gertpi_avg_time) + " milliseconds"
-    else:
-        print "invalid command"        #As of now only two commands are accepted: "start game" and "stop"
+    
+    
+    komm.send("start game")                       #sends the start command to clients
+    while (True):
+        parsestring(komm.recv(1024))        #end_time is received from the client as a string type
+       # print "Waiting to receive response time..."
+        #print "received response in " + end_time + " milliseconds"               
+        #total_time = total_time + int(end_time)          #Sum the total time by changing the string end_time into an integer
+       # round += 1                                       #increment the round
+        if (pifaceround == 10 and gertpiround == 10):                     #when round reaches 10, 10 responses have been received
+            break                           #So we stop
+        komm, addr = s.accept()                          #Wait for the client to be ready to send another value
+    piface_avg_time = pifacetotaltime/10.0              #Calculate the average time in milliseconds
+    print "Piface average reaction time is: " + str(piface_avg_time) + " milliseconds"
+    gertpi_avg_time = gertpitotaltime/10.0              #Calculate the average time in milliseconds
+    print "Gertpi average reaction time is: " + str(gertpi_avg_time) + " milliseconds"
+    
     print "Connection Terminated"      
     s.close()                          #End the program
 
@@ -78,9 +84,8 @@ def main():
     print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
     while (True):
-        print "Press Y to play, I for instructions, or Q to quit"
-        userinput=getch()
-        if (userinput=="i"):
+        userinput=raw_input("Press Y to play, I for instructions, or Q to quit: ")
+        if (userinput=="i" or "I"):
             instructions()
         elif (userinput=="y"):
             game()
