69096c24af718d6b5d08bd2a6862af7b1fb7fb88
Andrew Fleck <andrewfleck2@cmail.carleton.ca>
2013-10-01 15:27:27 -0400

    New ServerFile

--- Serverpi.py
+++ Serverpi.py
@@ -1,33 +1,36 @@
-gameon = False
-userinput = ""
+#Group B making a simple server application
+#involves networking and other experimental goodness
 
+import socket
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(("", 5050))
+s.listen(1)
+x = 0
+while(x == 0):
+	komm, addr = s.accept()
+	data = raw_input("Enter Command: ")
+	if data == "stop":
+		x = 1
+	elif data == "start game":
+		y = 0
+		total_time = 0
+		komm.send(data)
+		while (0 == 0):
+			end_time = komm.recv(1024)
+			print "Waiting to receive response time..."
+			print "received response in " + end_time + " milliseconds"
+			total_time = total_time + int(end_time)
+			y += 1
+			if y == 10:
+				break
+			komm, addr = s.accept()
+		avg_time = total_time/10.0
+		print "Average reaction time is: " + str(avg_time) + " milliseconds"
+	else:
+		print "invalid command"
+ 	x = 1
+print "Connection Terminated"
+s.close()
 
-def instructions()
-    print "There are two raspberrypi's with peripheral boards attached. These function as controllers"
-    print "This serverpi will send commands to them telling them which LEDs to light"        
-    print "It will count the time it takes to react to each LED by pressing the correct button on the controller pi's"
-    print "The winner will be determined by the lower average reaction time over 10 rounds"
-    print "MOST IMPORTANTLY, HAVE FUN!"
-    
-    
-def game()
-    while (gameon):
-        
-
-def main()
-
-    print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    print "!!!!Welcome to Group B's RaspberryPi Reaction Time Game!!!!"
-    print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
-    while (gameon==False):
-        print "Press Y to play, I for instructions, or Q to quit"
-        userinput=getch()
-        if (userinput=="i")
-            instructions()
-        if (userinput=="y")
-            gameon=True
-            game()
-        if (userinput=="q")
-            break        
