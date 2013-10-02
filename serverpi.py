4508fd600606e1d432cb8b846fd6a2b1d55e7ec0
chrissparrow <chrissparrow@cmail.carleton.ca>
2013-10-02 11:39:02 -0400

    Updated with formatting of comments

--- serverpi.py
+++ serverpi.py
@@ -60,21 +60,17 @@ def game():
     s.bind(("", 5050))                 #The server receives data from clients on port 5050
     s.listen(1)
     print "waiting for two players"
-    komm, addr = s.accept()            #The server waits for the client to connect                           
-    komm2, addr = s.accept()
-    raw_input("hit enter to start")
-    komm.send("start game")                       #sends the start command to clients
-    komm2.send("start game")                       #sends the start command to clients
+    komm, addr = s.accept()            #The server waits for the first client to connect                           
+    komm2, addr = s.accept()	       #The server waits for the second client to connect
+    raw_input("hit enter to start")    #The server is in control of the start of the game
+    komm.send("start game")            #sends the start command to clients
+    komm2.send("start game")                    
     while (True):
-        parsestring(komm.recv(1024))        #end_time is received from the client as a string type
-	parsestring(komm2.recv(1024))
-       # print "Waiting to receive response time..."
-        #print "received response in " + end_time + " milliseconds"               
-        #total_time = total_time + int(end_time)          #Sum the total time by changing the string end_time into an integer
-       # round += 1                                       #increment the round
-        if (pifaceround == 10 and gertpiround == 10):                     #when round reaches 10, 10 responses have been received
-            break                           #So we stop
-        komm, addr = s.accept()                          #Wait for the client to be ready to send another value
+        parsestring(komm.recv(1024))   			    #Processes response from the first client
+	parsestring(komm2.recv(1024))  			    #Processes response from second client (The order does not affect perceived results)
+        if (pifaceround == 10 and gertpiround == 10):       #when round reaches 10, 10 responses have been received
+            break                           	            #So we stop
+        komm, addr = s.accept()                          #Wait for the clients to be ready to send another value
     piface_avg_time = pifacetotaltime/10.0              #Calculate the average time in milliseconds
     print "Piface average reaction time is: " + str(piface_avg_time) + " milliseconds"
     gertpi_avg_time = gertpitotaltime/10.0              #Calculate the average time in milliseconds
